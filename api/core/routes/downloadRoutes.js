fa072418f7925c0882d22a408ae3568c3ed4c2fb
Giuliopime <giuliopime@gmail.com>
2021-01-25 16:37:20 +0100

    Added get requests without body support

--- api/core/routes/downloadRoutes.js
+++ api/core/routes/downloadRoutes.js
@@ -4,8 +4,14 @@ module.exports = (app) => {
     app.route('/spotify')
         .post(controller.spotify);
 
+    app.route('/spotify/:url')
+        .post(controller.spotifyWithParam);
+
     app.route('/youtube')
         .post(controller.youtube);
+
+    app.route('/youtube/:url')
+        .post(controller.youtubeWithParam);
 
     app.route('/download-info/:id')
         .get(controller.getDownloadInfo);
